d05c82f4a2eb8bdcd686f67a5a87419988dae368
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-11-25 13:35:32 -0300

    update: atualiza header para ficar mais charmoso

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="container d-flex flex-row justify-content-evenly border border-red">
+  <div class="container d-flex flex-row justify-content-evenly align-items-center ">
     <h1>Biblioteca</h1>
-    <input type="text">
+    <input type="text" class="form-control" placeholder="Pesquisar por título" />
     <button class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#CreateBook">adicionar</button>
   </div>
 </template>
+
+<style lang="css" scoped>
+input {
+  height: 40px;
+  width: 300px;
+}
+</style>
